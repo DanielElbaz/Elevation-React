ebda49de1ed25de65ade7eef7cd774a0f46b6900
Daniel <daniel.elbaz2000@gmail.com>
2025-08-06 14:59:12 +0300

    exercise 1 of intro

---
+++ intro/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+
+  const showCompany = (name,revenue) => { return companies.map(c => <div id={c.name}>{c.name} makes {c.revenue} every year</div>) }
+
+  let companies = [
+    { name: "Tesla", revenue: 140 },
+    { name: "Microsoft", revenue: 300 },
+    { name: "Google", revenue: 600 }
+  ]
+
+  return (
+    <div className="ex-space">
+      <h4 className='ex-title'>Exercise 1</h4>
+      <div className="exercise" id="ex-1">
+        {showCompany()}
+      </div>
+    </div>
+  );
+}
+
+export default App;
